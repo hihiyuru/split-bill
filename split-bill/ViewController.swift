af2430a178945710d6f6b2569ffe9b7be4d23ec2
Una <hihi555000@gmail.com>
2023-08-01 17:46:18 +0800

    Initial Commit

---
+++ split-bill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  split-bill
+//
+//  Created by 徐于茹 on 2023/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
