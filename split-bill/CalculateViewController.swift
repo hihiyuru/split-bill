7cc110cd99817ac075675c2887886327b612d77e
Una <hihi555000@gmail.com>
2023-08-03 00:51:33 +0800

    perf: 優化程式碼

--- split-bill/CalculateViewController.swift
+++ split-bill/CalculateViewController.swift
@@ -6,15 +6,14 @@
 //
 
 import UIKit
-import OSLog
 
 class CalculateViewController: UIViewController {
-    let logger = Logger()
     
     var currentNumber = 0
     var people = 1
     var percent = 0
     var isClear = false
+    
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var splitButton: UIButton!
@@ -22,36 +21,28 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var peopleLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var smallTatolLabel: UILabel!
-    
     @IBOutlet weak var smallPeopleLabel: UILabel!
     @IBOutlet weak var smallTipLabel: UILabel!
     @IBOutlet var percentButtons: [UIButton]!
     @IBOutlet var selectedTagImageView: [UIImageView]!
     @IBOutlet weak var splitBillButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setSlider()
-        setPercentButton()
-        setSelectedTagImageView()
-        splitButton.backgroundColor = UIColor(cgColor: CGColor(red: 15/255, green: 186/255, blue: 70/255, alpha: 1))
+        setupUI()
+    }
+    
+    func setupUI() {
+        setupSlider()
+        setupPercentButtons()
+        hideSelectedTagImageViews()
+        // 初始化 Split 按鈕樣式
+        splitButton.backgroundColor = UIColor(red: 15/255, green: 186/255, blue: 70/255, alpha: 1)
         selectedTagImageView[0].isHidden = false
     }
     
-    func setSelectedTagImageView() {
-        for tag in selectedTagImageView {
-            tag.isHidden = true
-        }
-    }
-    
-    func setPercentButton() {
-        for percent in percentButtons {
-            percent.layer.borderWidth = 1.0
-            percent.layer.borderColor = UIColor.gray.cgColor
-            percent.layer.cornerRadius = 5.0
-        }
-    }
-    
-    func setSlider(){
+    func setupSlider() {
+        // 設置人數選擇的 Slider 屬性
         personSlider.minimumValue = 1
         personSlider.maximumValue = 40
         personSlider.setMinimumTrackImage(UIImage(named: "green_track"), for: .normal)
@@ -59,32 +50,38 @@ class CalculateViewController: UIViewController {
         personSlider.setThumbImage(UIImage(named: "thumb"), for: .normal)
     }
     
-    func changeNumber(addNumber: Int) {
-        logger.log("按幾號\(addNumber)")
-        
-        let stringCurrentNumber: String = currentNumber == 0 && currentNumber / 10 < 1 ? "\(addNumber)" : "\(currentNumber)\(addNumber)"
-        
-        logger.log("多少\(stringCurrentNumber)")
-        if let customNumber = Int(stringCurrentNumber) {
-            currentNumber = customNumber
-            totalLabel.text = stringCurrentNumber
-            smallTatolLabel.text = "NT$\(stringCurrentNumber)"
+    func setupPercentButtons() {
+        // 設置百分比按鈕樣式
+        for percentButton in percentButtons {
+            percentButton.layer.borderWidth = 1.0
+            percentButton.layer.borderColor = UIColor.gray.cgColor
+            percentButton.layer.cornerRadius = 5.0
         }
-        
     }
     
-    func calculate() {
+    func hideSelectedTagImageViews() {
+        // 隱藏所有標籤圖像視圖
+        for tagImageView in selectedTagImageView {
+            tagImageView.isHidden = true
+        }
+    }
+    
+    // MARK: - UI Update
+    
+    func updateUI() {
+        // 計算並更新 UI
         let currentPercent = Float(percent) * (1 / 100)
         let currentTip = Float(currentNumber) * currentPercent
-        let splitAmount = (Float(currentNumber) +  currentTip) / Float(people)
-        logger.log("分攤的錢\(splitAmount)")
+        let splitAmount = (Float(currentNumber) + currentTip) / Float(people)
+        
         smallTipLabel.text = "NT$\(Int(round(currentTip)))"
         titleLabel.text = "‼️分攤金額"
         totalLabel.text = "\(Int(round(splitAmount)))"
     }
     
-    func initLayout() {
-        splitButton.backgroundColor = UIColor(cgColor: CGColor(red: 15/255, green: 186/255, blue: 70/255, alpha: 1))
+    func resetUI() {
+        // 重置 UI 到初始狀態
+        splitButton.backgroundColor = UIColor(red: 15/255, green: 186/255, blue: 70/255, alpha: 1)
         splitButton.setTitle("分帳", for: .normal)
         currentNumber = 0
         people = 1
@@ -96,25 +93,30 @@ class CalculateViewController: UIViewController {
         personSlider.value = 1
         peopleLabel.text = "1"
         titleLabel.text = "總額"
-        setSelectedTagImageView()
+        hideSelectedTagImageViews()
         selectedTagImageView[0].isHidden = false
         isClear = false
     }
     
+    // MARK: - Button Actions
+    
     @IBAction func onClickNumber(_ sender: UIButton) {
-        changeNumber(addNumber: sender.tag)
+        // 按下數字按鈕，更新目前輸入的金額
+        let number = sender.tag
+        currentNumber = currentNumber * 10 + number
+        totalLabel.text = "\(currentNumber)"
+        smallTatolLabel.text = "NT$\(currentNumber)"
     }
     
-    
     @IBAction func changePerson(_ sender: UISlider) {
-        peopleLabel.text = String(Int(sender.value))
-        logger.log("人數\(Int(sender.value))")
+        // 變更人數選擇，更新 UI
         people = Int(sender.value)
-        smallPeopleLabel.text = "\(Int(sender.value))"
+        peopleLabel.text = "\(people)"
+        smallPeopleLabel.text = "\(people)"
     }
     
     @IBAction func selectPercent(_ sender: UIButton) {
-        setSelectedTagImageView()
+        hideSelectedTagImageViews()
         switch sender.tag {
         case 0:
             selectedTagImageView[0].isHidden = false
@@ -132,16 +134,19 @@ class CalculateViewController: UIViewController {
     
     @IBAction func splitOrClear(_ sender: UIButton) {
         if currentNumber != 0, !isClear {
+            // 進行分攤計算
             sender.backgroundColor = UIColor.red
             sender.setTitle("重新計算", for: .normal)
-            calculate()
+            updateUI()
             isClear = true
-        } else if (isClear) {
-            
-            initLayout()
-            
+        } else if isClear {
+            // 重置 UI
+            resetUI()
         } else {
+            // 顯示提示訊息
             let alertController = UIAlertController(title: "提示", message: "請輸入總金額", preferredStyle: .alert)
+            
+            // 添加提示訊息的按鈕
             let okAction = UIAlertAction(title: "我知道了", style: .default, handler: nil)
             alertController.addAction(okAction)
             present(alertController, animated: true)
@@ -149,18 +154,24 @@ class CalculateViewController: UIViewController {
         }
     }
     
-    @IBAction func deletNumber(_ sender: Any) {
-        var stringCurrentNumber = "\(currentNumber)"
-        stringCurrentNumber.removeLast()
-        currentNumber = Int(stringCurrentNumber) ?? 0
-        totalLabel.text = currentNumber == 0 ? "0" : stringCurrentNumber
-        logger.log("??\(self.currentNumber)")
+    @IBAction func deleteNumber(_ sender: Any) {
+        // 刪除輸入的最後一個數字
+        if currentNumber >= 10 {
+            currentNumber /= 10
+        } else {
+            currentNumber = 0
+        }
+        totalLabel.text = "\(currentNumber)"
     }
     
-    @IBAction func allDelet(_ sender: Any) {
-        initLayout()
+    @IBAction func clearAll(_ sender: Any) {
+        // 重置所有 UI
+        resetUI()
     }
-
+    
     @IBAction func dismissKeyboard(_ sender: Any) {
+        // 收起鍵盤
+        view.endEditing(true)
     }
 }
+
